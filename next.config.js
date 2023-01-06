f8ddea644d5b88879e3f3b550e27b4bcc58211d3
Julien Vanharen <julien.vanharen@inria.fr>
2023-01-06 21:30:45 +0100

    Relative paths

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const isProd = process.env.NODE_ENV === 'production'
 /** @type {import('next').NextConfig} */
 module.exports = {
     reactStrictMode: true,
-    assetPrefix: isProd ? 'jvanhare.github.io/' : './',
+    assetPrefix: isProd ? 'https://jvanhare.github.io/' : './',
     publicRuntimeConfig: {
         buildYear: new Date().getFullYear(),
         buildMonth: new Date().toLocaleString("en-us", { month: "short" }),
